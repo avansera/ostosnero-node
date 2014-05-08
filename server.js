e5e15f98ff52a692cfd706db4d23f84e2f10c853
zac <zaccary.price@avansera.com>
2014-05-08 11:31:23 +0300

    add clusters and domain/okay error handling

--- server.js
+++ server.js
@@ -1,29 +1,61 @@
 'use strict';
 
-var express = require('express');
 
-/**
- * Main application file
- */
+var cluster	= require('cluster');
+var log		= require('npmlog');
+log.enableColor();
 
-// Set default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (cluster.isMaster) {
+	log.info('Cluster', "setting up master thread");
+	// Count the machine's CPUs
+	var cpuCount = require('os').cpus().length;
 
-// Application Config
-var config = require('./lib/config/config');
+	// Create a worker for each CPU
+	for (var i = 0; i < cpuCount; i += 1) {
+		cluster.fork();
+		log.info('Cluster', "Forking worker " + (i+1));
+	}
 
-var app = express();
+// Code to run if we're in a worker process
+} else {
 
-// Express settings
-require('./lib/config/express')(app);
+	var express = require('express');
 
-// Routing
-require('./lib/routes')(app);
+	/**
+	 * Main application file
+	 */
 
-// Start server
-app.listen(config.port, function () {
-	console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+	// Set default node environment to development
+	process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+	// Application Config
+	var config = require('./lib/config/config');
+
+	var app = express();
+
+	// Express settings
+	require('./lib/config/express')(app);
+
+	// Routing
+	require('./lib/routes')(app);
+
+	// Start server
+	app.listen(config.port, function () {
+		console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+	});
+
+	// Expose app
+	exports = module.exports = app;
+}
+
+
+// Listen for dying workers
+cluster.on('exit', function (worker) {
+
+	// Replace the dead worker,
+	// we're not sentimental
+	log.warn('Cluster', 'Worker ' + worker.id + ' has died', "Goodnight, sweet prince.");
+	cluster.fork();
+
 });
 
-// Expose app
-exports = module.exports = app;
