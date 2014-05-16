d75129bad5599d4c0d139484d706600dddfa9bbe
zac <zaccary.price@avansera.com>
2014-05-16 13:23:32 +0300

    fix login not validating passwords

--- public/app/scripts/controllers/login.js
+++ public/app/scripts/controllers/login.js
@@ -5,48 +5,6 @@ angular.module('App.Controllers').controller('LoginCtrl', function($q, $http, $s
     console.log($scope.loginForm);
 
 
-
-    /**
-     * function to submit login information to server
-     *
-     * @param user(string): user id, either email or username
-     * @param pass(string): given user password
-     *
-     * @returns deferred promise
-     */
-    function login(user, pass)
-    {
-        var dfd = $q.defer();
-        console.log(user + ", " + pass);
-        $http({
-            method: 'GET',
-            url: '../../api/login/',
-            params: {email: user, password: pass}
-        })
-            .success(function(data) {
-                console.log(data);
-
-                /**
-                 * if the 'success' json attribute is not found,
-                 * it is assumed that the returned value is an sql or php error.
-                 * The response to this would be to display a server error message.
-                 *
-                 */
-                if (data.success === undefined) {
-                    dfd.reject('No connection to server could be made');
-                } else {
-                    dfd.resolve(data);
-                }
-
-            })
-            .error(function(e) {
-                console.log(e);
-                dfd.reject();
-            });
-        return dfd.promise;
-
-    }
-
     $scope.hideError = function() {
         $scope.loginForm.$error.message = null;
     };
@@ -89,14 +47,23 @@ angular.module('App.Controllers').controller('LoginCtrl', function($q, $http, $s
 					}
 					else {
 						$rootScope.auth = true;
-						$location.path('/list/'/* + status.active_list*/);
+						$location.path('/list/');
 
 					}
             },
 			function(reason) {
 				$scope.busy = false;
                 //toggleSpinner($('#login-form button[type="submit"]'));
-                $scope.loginForm.$error.message = reason;
+				if(reason.err) {
+					switch(reason.err) {
+						case "ERR_PASS_NOMATCH":
+							$scope.loginForm.$error.message = "Wrong password";
+							break;
+					}
+				} else {
+					$scope.loginForm.$error.message = reason;
+				}
+
             });
 
         }
